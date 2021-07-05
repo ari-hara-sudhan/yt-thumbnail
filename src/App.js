6d006e965c4e3e44cfd6667ceb9fd382246b6e79
=ari <=ahshari2003@gmail.com>
2021-07-05 08:27:12 +0530

    header is done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="app">
       <Header/>
+      <div>
+        
+      </div>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -6,17 +6,38 @@ import SearchIcon from '@material-ui/icons/Search';
 import VideocamIcon from '@material-ui/icons/Videocam'
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications'
+import { IconButton } from '@material-ui/core';
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
+                <IconButton>
+                 <MenuIcon/>
+                </IconButton>
+                <img className="header__logo" src="https://static0.srcdn.com/wordpress/wp-content/uploads/2018/04/YouTube-Logo.jpg" alt="yt-logo"/>
 
             </div>
+            <div className="header__center">
+                <input/>
+                <SearchIcon fontSize="large" className="header__search"/>
+           </div>
+        <IconButton>
+        <MicIcon/>
+        </IconButton>
+           
             <div className="header__right">
-
-            </div>
-            <div>
-                
+                <IconButton>
+                <SearchIcon/>
+                </IconButton>
+                <IconButton>
+                <VideocamIcon/> 
+                </IconButton>
+                <IconButton>
+                <AppsIcon/>    
+                </IconButton>
+                <IconButton>
+                <NotificationsIcon/> 
+                </IconButton>
             </div>
         </div>
     )
