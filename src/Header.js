6ac0ef994dd9a1e5cab3ed411163919a229d5825
=ari <=ahshari2003@gmail.com>
2021-07-05 08:02:10 +0530

    working in header

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Header.css"
+import MenuIcon from '@material-ui/icons/Menu';
+import MicIcon from '@material-ui/icons/Mic';
+import SearchIcon from '@material-ui/icons/Search';
+import VideocamIcon from '@material-ui/icons/Videocam'
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications'
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+
+            </div>
+            <div className="header__right">
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
